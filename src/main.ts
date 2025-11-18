45d8361e3ea493f2d87391ed0602d7b603ddfae5
yamalpaca <dst.leopard@gmail.com>
2025-11-18 14:53:31 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import gameimg from "./gameicons.png";
 import meterimg from "./meterimg.png";
 import pntimg from "./pntimg.png";
 import "./style.css";
-//hi
 
 const customFont = new FontFace("SeuratPro", "/src/FOT-SeuratPro-B.otf");
 document.fonts.add(customFont);
